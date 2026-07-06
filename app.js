4d1bb7047d4fa673aad47780e55eae607ce641ac
lucaslee71516@gmail.com <lucaslee71516@gmail.com>
2026-07-06 13:30:41 -0300

    uuuiuiuuiiu

--- app.js
+++ app.js
@@ -237,6 +237,7 @@ function loginWithGoogle() {
         });
 }
 
+
 function registerUser() { 
     const nombre = document.getElementById('reg-nombre').value.trim();
     const apellido = document.getElementById('reg-apellido').value.trim();
